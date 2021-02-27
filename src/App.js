f163ad03e0516653faf0e7384cfdd189f8244f0b
Tomoya Ikegami <60150746+tom0418@users.noreply.github.com>
2021-02-27 16:57:30 +0900

    add components TodoList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Todo from './Todo'
 import AddTodo from './components/AddTodo'
+import TodoList from './components/TodoList'
 
 const App = () => {
   return (
     <div>
       <Todo />
       <AddTodo />
+      <TodoList />
     </div>
   )
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TodoList = () => {
+  return (
+    <ul>
+      <li>test todo item1</li>
+      <li>test todo item2</li>
+      <li>test todo item3</li>
+    </ul>
+  )
+}
+
+export default TodoList
