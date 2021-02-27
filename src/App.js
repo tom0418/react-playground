6a05b4c43edb68e3a720760f38e7eece1719023d
Tomoya Ikegami <60150746+tom0418@users.noreply.github.com>
2021-02-27 16:49:40 +0900

    add components AddTodo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Todo from './Todo'
+import AddTodo from './components/AddTodo'
 
 const App = () => {
   return (
     <div>
       <Todo />
+      <AddTodo />
     </div>
   )
 }
---
+++ src/components/AddTodo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class AddTodo extends React.Component {
+  render() {
+    return (
+      <div>
+        <input />
+        <button>Add Todo</button>
+      </div>
+    )
+  }
+}
+
+export default AddTodo
