93a90d2106129cdc8a772424de845a9e8be6df4e
Tomoya Ikegami <60150746+tom0418@users.noreply.github.com>
2021-02-27 19:04:11 +0900

    add ChangeEvnet

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -3,11 +3,30 @@ import { connect } from 'react-redux'
 import { addTodo} from '../redux/actions'
 
 class AddTodo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { input: '' }
+  }
+
+  updateInput = input => {
+    this.setState({ input })
+  }
+
+  handleAddTodo = () => {
+    this.props.addTodo(this.state.input)
+    this.setState({ input: '' })
+  }
+
   render() {
     return (
       <div>
-        <input />
-        <button>Add Todo</button>
+        <input
+          onChange={e => this.updateInput(e.target.value)}
+          value={this.state.input}
+        />
+        <button onClick={this.handleAddTodo}>
+          Add Todo
+        </button>
       </div>
     )
   }
